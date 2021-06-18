c160923401ec99256e3f9273dc70cdf71d556a40
werto87 <huhulasmichinruhe@web.de>
2021-06-18 21:41:39 +0200

    Update concept.hxx

--- confu_json/concept.hxx
+++ confu_json/concept.hxx
@@ -1,10 +1,9 @@
-#
-# Copyright (c) 2021 Waldemar Schneider (w-schneider1987 at web dot de)
-#
-# Distributed under the Boost Software License, Version 1.0.
-# (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-#
-
+//
+// Copyright (c) 2021 Waldemar Schneider (w-schneider1987 at web dot de)
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
 #ifndef A0BAA2CA_6E62_4337_B0BE_F4AFE5469841
 #define A0BAA2CA_6E62_4337_B0BE_F4AFE5469841
 #include <boost/algorithm/string.hpp>
