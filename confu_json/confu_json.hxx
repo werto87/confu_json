9f7c1ab5451426f34338f7d360d9c425e78b33d7
werto87 <huhulasmichinruhe@web.de>
2021-06-18 21:39:19 +0200

    Update confu_json.hxx

--- confu_json/confu_json.hxx
+++ confu_json/confu_json.hxx
@@ -1,3 +1,9 @@
+#
+# Copyright (c) 2021 Waldemar Schneider (w-schneider1987 at web dot de)
+#
+# Distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+#
 #ifndef D8CCCDE6_2E39_45B4_B2D8_A65E0438B8B2
 #define D8CCCDE6_2E39_45B4_B2D8_A65E0438B8B2
 
