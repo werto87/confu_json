4e2ab0e1f297bac384ca9c1d0db5da22e62fe974
werto87 <huhulasmichinruhe@web.de>
2021-11-21 13:45:23 +0100

    optional vector support - refactoring put code into handleArray function

--- confu_json/to_json.hxx
+++ confu_json/to_json.hxx
@@ -16,7 +16,8 @@
 #include <magic_enum.hpp>
 namespace confu_json
 {
-
+template <class T> void handlePair (boost::json::object &result, T const &member, std::string const &memberName);
+template <class T> bool handleOptional (boost::json::object &result, T const &t, std::string const &memberName);
 template <class T> boost::json::object to_json (T const &t);
 
 template <class T>
@@ -121,7 +122,7 @@ handleOptional (boost::json::object &result, T const &t, std::string const &memb
                   // handle optional case when vector is optional
                   using namespace boost::json;
                   array tmp;
-                  confu_json::handleArray (tmp, t.value ());
+                  handleArray (tmp, t.value ());
                   result[memberName] = tmp;
                 }
               else
@@ -283,69 +284,8 @@ to_json (T const &t)
       }
     else if constexpr (IsArray<currentType> and not IsPrintable<currentType>)
       {
-        using elementType = std::remove_reference_t<decltype (member.front ())>;
         array result;
-        for (auto const &element : member)
-          {
-            object tmp;
-            if constexpr (IsOptional<elementType>)
-              {
-                using optionalType = std::remove_reference_t<decltype (element.value ())>;
-                if constexpr (boost::fusion::traits::is_sequence<optionalType>::value)
-                  {
-                    if (handleOptional (tmp, element, std::string{ type_name<elementType> () }))
-                      {
-                        result.push_back (tmp);
-                      }
-                    else
-                      {
-                        result.push_back (nullptr);
-                      }
-                  }
-                else
-                  {
-                    if (element.has_value ())
-                      {
-                        if constexpr (std::is_enum_v<optionalType>)
-                          {
-                            tmp[std::string{ type_name<optionalType> () }] = std::string{ magic_enum::enum_name (element.value ()) };
-                            std::cout << tmp << std::endl;
-                            result.emplace_back (tmp);
-                          }
-                        else
-                          {
-                            result.emplace_back (element.value ());
-                          }
-                      }
-                    else
-                      {
-                        result.push_back (nullptr);
-                      }
-                  }
-              }
-            else if constexpr (IsPair<elementType>)
-              {
-                handlePair (tmp, element, std::string{ type_name<elementType> () });
-                result.push_back (tmp.at (std::string{ type_name<elementType> () }).as_array ());
-              }
-            else if constexpr (boost::fusion::traits::is_sequence<elementType>::value)
-              {
-                tmp[std::string{ type_name<elementType> () }] = to_json (element);
-                result.push_back (tmp);
-              }
-            else
-              {
-                if constexpr (std::is_enum_v<elementType>)
-                  {
-                    tmp[std::string{ type_name<elementType> () }] = std::string{ magic_enum::enum_name (element) };
-                    result.push_back (tmp);
-                  }
-                else
-                  {
-                    result.emplace_back (element);
-                  }
-              }
-          }
+        handleArray (result, member);
         obj[memberName] = result;
       }
     else
--- confu_json/to_object.hxx
+++ confu_json/to_object.hxx
@@ -17,12 +17,15 @@ namespace confu_json
 
 template <typename T> T to_object (boost::json::value const &_value); // pre declare
 
+template <typename T, typename U> void handleOptional (T &t, U &_value, std::string const &name);
+
 template <typename T, typename U>
 void
 handleArray (T &t, U &_value)
 {
   using namespace boost::json;
   using someTypeOtherType = std::remove_reference_t<decltype (t.front ())>;
+
   if constexpr (IsOptional<someTypeOtherType>)
     {
       using optionalType = std::remove_reference_t<decltype (t.front ().value ())>;
@@ -191,7 +194,7 @@ handleArray (T &t, U &_value)
         }
       else if constexpr (std::is_enum_v<someTypeOtherType>)
         {
-          for (value const &element : t.as_array ())
+          for (value const &element : _value.as_array ())
             {
               auto result = std::string{ type_name<someTypeOtherType> () };
               auto enumOptional = magic_enum::enum_cast<someTypeOtherType> (element.as_object ().at (result).as_string ().c_str ());
@@ -201,7 +204,7 @@ handleArray (T &t, U &_value)
                 }
               else
                 {
-                  std::cout << type_name<someTypeOtherType> () << ": not supported enum value: " << t.as_string ().c_str () << std::endl;
+                  std::cout << type_name<someTypeOtherType> () << ": not supported enum value: " << _value.as_string ().c_str () << std::endl;
                 }
             }
         }
@@ -265,7 +268,6 @@ handleOptional (T &t, U &_value, std::string const &name)
     }
   else if constexpr (IsArray<optionalType> and not IsPrintable<optionalType>)
     {
-      // TODO add array support
       auto tmp = optionalType{};
       handleArray (tmp, jsonDataForMember);
       t = tmp;
@@ -294,8 +296,6 @@ to_object (boost::json::value const &_value)
     else if constexpr (std::is_unsigned<currentType>::value)
       {
         // TODO think about unsigned support how to save a number in a json so we can say its an unsigned for example we have 2 in a json is this unsigned or not?
-        std::cout << memberName << std::endl;
-        std::cout << jsonDataForMember << std::endl;
         member = jsonDataForMember.as_uint64 ();
       }
     else if constexpr (IsOptional<currentType>)
@@ -313,193 +313,7 @@ to_object (boost::json::value const &_value)
       {
         if constexpr (IsArray<currentType>)
           {
-            using someTypeOtherType = std::remove_reference_t<decltype (member.front ())>;
-            if constexpr (IsOptional<someTypeOtherType>)
-              {
-                using optionalType = std::remove_reference_t<decltype (member.front ().value ())>;
-                if constexpr (boost::fusion::traits::is_sequence<optionalType>::value)
-                  {
-                    for (value const &element : jsonDataForMember.as_array ())
-                      {
-                        if (not element.is_null ())
-                          {
-                            auto temp = someTypeOtherType{};
-                            handleOptional (temp, element, std::string{ type_name<someTypeOtherType> () });
-                            member.push_back (temp);
-                          }
-                        else
-                          {
-                            member.push_back (someTypeOtherType{});
-                          }
-                      }
-                  }
-                else
-                  {
-                    for (value const &element : jsonDataForMember.as_array ())
-                      {
-                        if (not element.is_null ())
-                          {
-                            if constexpr (std::is_same<bool, optionalType>::value)
-                              {
-                                member.push_back (element.as_bool ());
-                              }
-                            else if constexpr (std::is_signed<optionalType>::value)
-                              {
-                                member.push_back (element.as_int64 ());
-                              }
-                            else if constexpr (std::is_unsigned<optionalType>::value)
-                              {
-                                member.push_back (element.as_uint64 ());
-                              }
-                            else if constexpr (IsPrintable<optionalType>)
-                              {
-                                if (element.kind () == kind::string) member.push_back (std::string{ element.as_string ().c_str () });
-                                else
-                                  {
-                                    member.push_back (std::string{});
-                                  }
-                              }
-                            else if constexpr (std::is_enum_v<optionalType>)
-                              {
-
-                                auto result = std::string{ type_name<optionalType> () };
-                                auto enumOptional = magic_enum::enum_cast<optionalType> (element.as_object ().at (result).as_string ().c_str ());
-                                if (enumOptional)
-                                  {
-                                    member.push_back (enumOptional.value ());
-                                  }
-                                else
-                                  {
-                                    std::cout << type_name<optionalType> () << ": not supported enum value: " << jsonDataForMember.as_string ().c_str () << std::endl;
-                                  }
-                              }
-                          }
-                        else
-                          {
-                            member.push_back (someTypeOtherType{});
-                          }
-                      }
-                  }
-              }
-            else if constexpr (IsPair<someTypeOtherType>)
-              {
-                for (auto &element : jsonDataForMember.as_array ())
-                  {
-                    auto result = someTypeOtherType{};
-                    using firstType = std::remove_reference_t<decltype (result.first)>;
-                    using secondType = std::remove_reference_t<decltype (result.second)>;
-                    if constexpr (IsOptional<firstType>)
-                      {
-                        if (not element.at (0).is_null ())
-                          {
-                            handleOptional (result.first, element.at (0), std::string{ type_name<firstType> () });
-                          }
-                      }
-                    else
-                      {
-                        if constexpr (std::is_enum_v<firstType>)
-                          {
-                            auto typeName = std::string{ type_name<firstType> () };
-                            auto enumOptional = magic_enum::enum_cast<firstType> (element.at (0).at (typeName).as_string ().c_str ());
-                            if (enumOptional)
-                              {
-                                result.first = enumOptional.value ();
-                              }
-                            else
-                              {
-                                std::cout << type_name<firstType> () << ": not supported enum value: " << element.as_string ().c_str () << std::endl;
-                              }
-                          }
-                        else if constexpr (std::is_same<bool, firstType>::value)
-                          {
-                            result.first = element.at (0).as_bool ();
-                          }
-                        else if constexpr (std::is_signed<firstType>::value || std::is_unsigned<firstType>::value)
-                          {
-                            result.first = element.at (0).as_int64 ();
-                          }
-                        else if constexpr (IsPrintable<firstType>)
-                          {
-                            if (element.at (0).kind () == kind::string) result.first = element.at (0).as_string ().c_str ();
-                          }
-                        else
-                          {
-                            result.first = to_object<firstType> (element.at (0).at (type_name<firstType> ()));
-                          }
-                      }
-
-                    if constexpr (IsOptional<secondType>)
-                      {
-                        if (not element.at (1).is_null ())
-                          {
-                            handleOptional (result.second, element.at (1), std::string{ type_name<secondType> () });
-                          }
-                      }
-                    else
-                      {
-                        if constexpr (std::is_enum_v<secondType>)
-                          {
-                            auto typeName = std::string{ type_name<secondType> () };
-                            auto enumOptional = magic_enum::enum_cast<secondType> (element.at (1).at (typeName).as_string ().c_str ());
-                            if (enumOptional)
-                              {
-                                result.second = enumOptional.value ();
-                              }
-                            else
-                              {
-                                std::cout << type_name<secondType> () << ": not supported enum value: " << element.as_string ().c_str () << std::endl;
-                              }
-                          }
-                        else if constexpr (std::is_same<bool, secondType>::value)
-                          {
-                            result.second = element.at (1).as_bool ();
-                          }
-                        else if constexpr (std::is_signed<secondType>::value || std::is_unsigned<secondType>::value)
-                          {
-                            result.second = element.at (1).as_int64 ();
-                          }
-                        else if constexpr (IsPrintable<secondType>)
-                          {
-                            if (element.at (1).kind () == kind::string) result.second = element.at (1).as_string ().c_str ();
-                          }
-                        else
-                          {
-                            result.second = to_object<secondType> (element.at (1).at (type_name<secondType> ()));
-                          }
-                      }
-                    member.push_back (result);
-                  }
-              }
-            else
-              {
-                if constexpr (boost::fusion::traits::is_sequence<someTypeOtherType>::value)
-                  {
-                    for (value const &element : jsonDataForMember.as_array ())
-                      {
-                        member.push_back (to_object<someTypeOtherType> (element.as_object ().at (type_name<someTypeOtherType> ())));
-                      }
-                  }
-                else if constexpr (std::is_enum_v<someTypeOtherType>)
-                  {
-                    for (value const &element : jsonDataForMember.as_array ())
-                      {
-                        auto result = std::string{ type_name<someTypeOtherType> () };
-                        auto enumOptional = magic_enum::enum_cast<someTypeOtherType> (element.as_object ().at (result).as_string ().c_str ());
-                        if (enumOptional)
-                          {
-                            member.push_back (enumOptional.value ());
-                          }
-                        else
-                          {
-                            std::cout << type_name<someTypeOtherType> () << ": not supported enum value: " << jsonDataForMember.as_string ().c_str () << std::endl;
-                          }
-                      }
-                  }
-                else
-                  {
-                    member = value_to<currentType> (jsonDataForMember);
-                  }
-              }
+            handleArray (member, jsonDataForMember);
           }
         else if constexpr (IsPair<currentType>)
           {
