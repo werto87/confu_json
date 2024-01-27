1dcb58e3c07b838f8e81b6877a05799bb6c6ba83
werto87 <huhulasmichinruhe@web.de>
2024-01-27 17:30:08 +0900

    boost version bump. cmake version reduce to fit conan center ci

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ class Project(ConanFile):
     def requirements(self):
         self.requires("catch2/2.13.7")
         self.requires("magic_enum/[>=0.9.5 <10]")
-        self.requires("boost/1.83.0")
+        self.requires("boost/1.84.0")
 
 
 
